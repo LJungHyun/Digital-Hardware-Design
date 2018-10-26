575ef7364eb83bb163f8e368768602f2f54ae190
LJungHyun <44283121+LJungHyun@users.noreply.github.com>
2018-10-27 01:11:21 +0900

    HW3 end
    처음 잘못설계한 error와 수정한 Mealy

--- FSM/Test_FSM.v
+++ FSM/Test_FSM.v
@@ -1,11 +1,9 @@
 module Test_FSM;
-	reg nRESET; // RESET
-	reg clk; // CLOCK
-	reg in; // input
-	wire out; // ouput
+	reg nRESET; 				// 리셋
+	reg clk;						// 클럭
+	reg in; 						// 입력 신호
+	wire out;					// 출력
 	
-	always #100 clk = ~clk; // clock cycle은 200마다 상승 
-
 	Mealy U0 
 	(
 		.nRESET(nRESET),
@@ -13,38 +11,33 @@ module Test_FSM;
 		.in(in),
 		.out(out)
 	);
-/*
-	moore U0 
-	(
-		.nRESET(nRESET),
-		.clk(clk),
-		.in(in),
-		.out(out)
-	);
-*/
+	
+	always #50 clk = ~clk; // clock cycle은 200마다 상승 
+
+	
 	initial begin
 		clk = 1'b1;
 		nRESET = 1'b1;
 		
-		#200 in = 1'b1;
-		#200 in = 1'b1;
-		#200 in = 1'b1;
-		#200 in = 1'b1; // 1의 입력이 4번 들어올 때 출력 1이 나와야 한다.
-		#200 in = 1'b1; // 여기까지 1의 입력이 5번 들어온다. (출력 1)
-		#200 in = 1'b0; // 0의 입력 (출력 0)
-		#200 in = 1'b0; 
-		#200 in = 1'b1; // 1 입력
-
-		#200 in = 1'b0;
-		#200 in = 1'b0;
-		#200 in = 1'b0;
-		#200 in = 1'b0; // 0 입력 4번이 들어올 때 출력 1이 나와야 한다.
-		#200 in = 1'b0; // 0 입력 5번 유지할 때 (출력 1)
-		#200 in = 1'b1;
-		#200 in = 1'b1; // 1의 입력 (출력 0)
-		#200 in = 1'b0;
+		#100 in = 1'b0;
+		#100 in = 1'b0;
+		#100 in = 1'b1;
 		
-		#300
+		#100 in = 1'b0;
+		#100 in = 1'b0;
+		#100 in = 1'b0;
+		#100 in = 1'b0;
+		
+		#100 in = 1'b1;
+		#100 in = 1'b1;
+		#100 in = 1'b1;
+		#100 in = 1'b1;
+		#100 in = 1'b1;
+		
+		#100 in = 1'b0;
+		#100 in = 1'b0;
+		
+		#100
 		$finish;
 	end
 endmodule
